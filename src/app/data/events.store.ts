f420184c53f78a0bf418e3eb1c4c2bac15b7d840
Romain Nugou <romain.nugou@logient.com>
2025-01-31 16:27:47 -0500

    Add dark mode

--- src/app/data/events.store.ts
+++ src/app/data/events.store.ts
@@ -8,5 +8,5 @@ export const events: any = [
   {name: 'Event 7', date: '2020-02', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
   {name: 'Event 8', date: '2020-03', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
   {name: 'Event 9', date: '2020-04', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
-  {name: 'Event 10', date: '2020-05', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']}
+  {name: 'Event 10', date: '2020-05', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg', 'test.jpg', 'test.jpg']}
 ];
